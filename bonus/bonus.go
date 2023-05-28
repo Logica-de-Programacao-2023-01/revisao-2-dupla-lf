1e0fd24e1df47262d341d231a80c565b3d9a09de
Felipe Augusto <126472483+felpino2@users.noreply.github.com>
2023-05-28 00:15:12 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -6,6 +6,24 @@ import "errors"
 //vai de cidadeAi para cidadeBi. Retorne a cidade de destino, ou seja, a cidade sem nenhum caminho que saia dela.
 
 func Destino(caminhos [][2]string) (string, error) {
-	// Seu código aqui
-	return "", errors.New("Not implemented yet")
+	if len(caminhos) == 0 {
+		return "", errors.New("Not implemented yet")
+	}
+	mapinha := make(map[string]int)
+	if len(caminhos) == 1 {
+		return caminhos[0][1], nil
+	}
+
+	for i := 0; i < len(caminhos); i++ {
+		mapinha[string(caminhos[i][0])]++
+		for j := 0; j < 2; j++ {
+			mapinha[string(caminhos[i][j])]++
+		}
+	}
+	for chave, valoramt := range mapinha {
+		if valoramt == 1 {
+			return chave, nil
+		}
+	}
+	return "", errors.New("Ó O GAS")
 }
